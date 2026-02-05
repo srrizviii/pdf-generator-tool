81fc7616d5629aa66215d0e75720b8281092a2ce
srrizviii <rayanr0041@gmail.com>
2026-02-04 23:24:49 -0700

    Started working on PDF generator tool

---
+++ main.py
@@ -0,0 +1,17 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation="portrait", unit="mm", format="A4")
+pdf.add_page()
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello there", align="L", ln=1, border=1)
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="how is it going", align="L", ln=1, border=1)
+
+pdf.add_page()
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello there", align="L", ln=1, border=1)
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="how is it going", align="L", ln=1, border=1)
+
+
+pdf.output("output.pdf")
