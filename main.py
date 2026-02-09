b01d4243cf37cd928c294b9cf64aca885164659a
srrizviii <rayanr0041@gmail.com>
2026-02-09 13:47:40 -0700

    Finished PDF generator by adding lined pages

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ for index, row in dataframe.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0,0,0)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
-    
-    pdf.ln(260)
+    for y in range(20 ,298, 10):
+        pdf.line(10, y, 200, y)
+
+    pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.ln(270)
+        pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+        for y in range(20 ,298, 10):
+            pdf.line(10, y, 200, y)
     
 pdf.output("output.pdf")
